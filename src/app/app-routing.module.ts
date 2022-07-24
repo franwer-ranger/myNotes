566125f0a619a69e484dfe1f2c286f0cf1a5fcb9
Fran <fran@aurorajobs.io>
2022-07-24 12:58:17 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotesComponent } from './components/notes/notes.component';
+
+const routes: Routes = [
+  {
+    path: 'notes',
+    component:NotesComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
